8fbe108b1a6a4233da2a142135ea6edc82c75200
ShantanuDas15 <shantanucool1361@gmail.com>
2026-09-09 06:05:08 +0530

    Triplet Sum to Zero Optimal Solution

--- Two-Pointer Approach/Triplet_Sum_To_Zero.py
+++ Two-Pointer Approach/Triplet_Sum_To_Zero.py
@@ -1,11 +1,34 @@
-a = [1, 2, 3, 4]
-k = 2
+a = [-1, 0, 1, 2, -1, -4]
 
-window_sum = sum(a[:k])
-max_sum = window_sum
+triplet = []
 
-for i in range(k, len(a)):
-    window_sum += a[i] - a[i - k]
-    max_sum = max(max_sum, window_sum)
+a.sort()
 
-print(max_sum)
+for i in range(len(a) - 2):
+    if i > 0 and a[i] == a[i - 1]:
+        continue
+
+    low = i + 1
+    high = len(a) - 1
+
+    while low < high:
+        total = a[i] + a[low] + a[high]
+
+        if total == 0:
+            triplet.append([a[i], a[low], a[high]])
+            left = a[low]
+            right = a[high]
+
+            while low < high and left == a[low]:
+                low += 1
+
+            while low < high and a[high] == right:
+                high -= 1
+
+        elif total > 0:
+            high -= 1
+
+        else:
+            low += 1
+
+print(triplet)
